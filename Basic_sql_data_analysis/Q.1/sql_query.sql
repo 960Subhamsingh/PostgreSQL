58496e8487aaf9afd9c4b06b798410f6e2be5c87
960subhamsingh <kumarsubham0734@gmail.com>
2024-07-08 23:50:53 +0530

    add query

--- Basic_sql_data_analysis/Q.1/sql_query.sql
+++ Basic_sql_data_analysis/Q.1/sql_query.sql
@@ -2,6 +2,8 @@
 select * from sales 
 select * from  menu
 
+update menu set product_name = 'bread'  where product_id =3
+
 --1. What is the total amount each customer spent at the restaurant?
 
 SELECT 	customer_id  ,sum(price) AS TOTAL FROM SALES 
@@ -13,7 +15,33 @@ GROUP BY customer_id;
 select customer_id,  count(order_date) as days from sales  group by customer_id
 
 -- 3. What was the first item from the menu purchased by each customer?
+	
+WITH cte_first_item AS
+(select customer_id, product_name, 
+	Row_number() over(partition by sales.customer_id order by
+	(sales.order_date , sales.product_id )) as RN
+	from sales join menu on sales.product_id = menu.product_id	 
+	) select customer_id, product_name from cte_first_item where RN = 1
+	
+-- 4. What is the most purchased item on the menu and how many times was it
+	purchased by all customers?
 
--- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+select product_name ,  count(menu.product_id) from menu join sales
+	on sales.product_id = menu.product_id 
+	group by product_name limit 1;
+
+-- 5. Which item was the most popular for each customer?
+
+WITH CTE_MOST_POPULAR AS
+(select customer_id , product_name ,
+	Rank() over(partition by customer_id order by count(menu.product_id)) as rk
+	from sales join menu on sales.product_id = menu.product_id
+	group by customer_id , product_name 
+	)
+select * from CTE_MOST_POPULAR where rk = 1
+
+
+-- 6. Which item was purchased first by the customer after they became a member?
+
 
 
