858af4b58268f3f37e12660a9a84cf45d44bc7c2
960subhamsingh <kumarsubham0734@gmail.com>
2024-07-08 18:29:04 +0530

    sql_query add one question

--- Basic_sql_data_analysis/Q.1/sql_query.sql
+++ Basic_sql_data_analysis/Q.1/sql_query.sql
@@ -10,4 +10,10 @@ GROUP BY customer_id;
 
 -- 2. How many days has each customer visited the restaurant?
 
-select * from sales 
+select customer_id,  count(order_date) as days from sales  group by customer_id
+
+-- 3. What was the first item from the menu purchased by each customer?
+
+-- 4. What is the most purchased item on the menu and how many times was it purchased by all customers?
+
+
